7cce576ef4c7fb2c90dc5604857102afe88c0b61
Dmitriy Rykov <dimon.man.18@gmail.com>
2022-08-13 16:32:20 +0300

    +

--- lesson3_6step3.py
+++ lesson3_6step3.py
@@ -6,14 +6,6 @@ from selenium.webdriver.support import expected_conditions as EC
 import time
 import math
 
-@pytest.fixture(scope="function")
-def browser():
-    print("\nstart browser for test..")
-    browser = webdriver.Chrome()
-    browser.implicitly_wait(5)
-    yield browser
-    print("\nquit browser..")
-    browser.quit()
 
 @pytest.mark.parametrize("step", ["236895", "236896", "236897", "236898", "236899", "236903", "236904", "236905"])
 def test_aliens(step, browser):
